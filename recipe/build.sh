ecec1dbbe7b86e228dff7fa8edfc3b01e7656c70
Matthew Pitkin <matthew.pitkin@ligo.org>
2023-01-06 17:09:10 +0000

    Try a native build as suggested in https://github.com/conda-forge/tempo2-feedstock/pull/17#issuecomment-1304928751

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,33 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./tests/gtest-1.7.0/build-aux
 #echo "CXXFLAGS $CXXFLAGS"
 
 export TEMPO2=$PREFIX/share/tempo2
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  (
+    # Make native build of tempo2
+    mkdir -p native-build
+    pushd native-build
+    
+    unset MACOSX_DEPLOYMENT_TARGET
+    
+    export CC=$CC_FOR_BUILD
+    export host_alias=$build_alias
+    
+    cp -r ../bootstrap ../configure.ac ../*.c ../*.C ../*.h ../*.sh ../*.txt ../T2runtime
+    
+    ./bootstrap
+    ./configure --prefix=$BUILD_PREFIX --disable-local --disable-psrhome PGPLOT_DIR=$PREFIX/include/pgplot
+    make -j${CPU_COUNT}
+
+    popd
+  )
+  export PATH=`pwd`/native-build:$PATH
+fi
+
 ./bootstrap
 ./configure --prefix=$PREFIX --disable-local --disable-psrhome PGPLOT_DIR=$PREFIX/include/pgplot
 make -j${CPU_COUNT}
+
 make install
 make -j${CPU_COUNT} plugins
 make plugins-install
