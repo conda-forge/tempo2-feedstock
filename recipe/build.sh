3196874a1f80459dd75703a761eaefce83b6977a
UENO, M. <eunosium-1128@live.jp>
2026-08-19 03:06:45 +0900

    Add CFLAGS for GNU17 standard

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@
 #echo "CXXFLAGS $CXXFLAGS"
 
 export TEMPO2=$PREFIX/share/tempo2
+export CFLAGS="${CFLAGS} -std=gnu17"
 
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./tests/gtest-1.7.0/build-aux
